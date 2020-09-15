1fed7df4e7a8a29b55167df0d7e7adf8c3bd2986
Your Name <you@example.com>
2020-09-14 23:49:34 -0400

    adding more impls

--- ecomerceapp/src/main/java/com/example/ecomerceapp/service/BoatServiceImpl.java
+++ ecomerceapp/src/main/java/com/example/ecomerceapp/service/BoatServiceImpl.java
@@ -1,4 +1,37 @@
 package com.example.ecomerceapp.service;
 
-public class BoatServiceImpl {
+
+import com.example.ecomerceapp.model.Boat;
+import com.example.ecomerceapp.repository.BoatRepository;
+import com.example.ecomerceapp.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoatServiceImpl implements BoatService {
+    @Autowired
+    BoatRepository boatRepository;
+
+
+    @Override
+    public Iterable<Boat> listAllBoats() {
+        return boatRepository.findAll();
+    }
+
+    @Override
+    public Boat createBoat(Boat boat) {
+        return boatRepository.save(boat);
+    }
+
+    @Override
+    public Boat getBoatById(Long id) {
+        return boatRepository.findById(id).get();
+    }
+
+    @Override
+    public HttpStatus deleteBoatById(Long id) {
+        boatRepository.deleteById(id);
+        return HttpStatus.OK;
+    }
 }
